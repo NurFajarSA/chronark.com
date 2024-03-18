ae57e73c096f2b3acea690ca7996c27b6efb321b
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-03-18 21:36:37 +0700

    feat: fajar launch

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://linkedin.com/in/nfajarsa",
+		label: "LinkedIn",
+		handle: "Nur Fajar",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:nur.fajar11@ui.ac.id",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "nur.fajar11@ui.ac.id",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/NurFajarSA",
 		label: "Github",
-		handle: "chronark",
+		handle: "NurFajarSA",
 	},
 ];
 
